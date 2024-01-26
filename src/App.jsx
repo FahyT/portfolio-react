f7d1ad83dabd335382853d7e0ca838d6185bb2b1
Tom Fahy <thomas.fahy9@gmail.com>
2024-01-26 17:29:33 +0000

    add header navbar and basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import React from "react";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
 
-export default function App() {}
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      {/* Wrap Route elements in a Routes component */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="projects"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Projects
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
