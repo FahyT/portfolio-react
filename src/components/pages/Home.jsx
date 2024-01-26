d74960e3cd0d67c88ffb109b8cbabc391a02cbef
Tom Fahy <thomas.fahy9@gmail.com>
2024-01-26 18:43:22 +0000

    add some css and image to Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,19 +1,10 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className="p-5 mb-4 d-flex align-items-center jumbotron">
+      <div className="container-fluid py-5">
+        <h1 className="display-5 fw-bold">Tom Fahy</h1>
+        <p className="fs-4">DEVELOPER EXTRORDINAIRE</p>
+      </div>
     </div>
   );
 }
